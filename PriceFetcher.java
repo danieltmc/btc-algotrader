ca8973ee5a0a805ada0e31791c3f279c383e8624
danieltmc <me@danieltmc.com>
2019-08-23 20:24:02 -0400

    Basic connection to PostgreSQL database

---
+++ PriceFetcher.java
@@ -0,0 +1,19 @@
+import java.sql.*;
+
+public class PriceFetcher
+{
+	String api = "https://api.pro.coinbase.com";
+	String url = "jdbc:postgresql:btc";
+	
+	public static void main(String[] args)
+	{
+		try
+		{
+			
+		}
+		catch (Exception e)
+		{
+			e.printStackTrace();
+		}
+	}
+}
