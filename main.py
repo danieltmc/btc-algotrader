da5d459c94f94d0ea82dfa241721f4c3470a1254
danieltmc <me@danieltmc.com>
2019-12-24 11:23:00 -0500

    Add logic to load/create dataframe

--- main.py
+++ main.py
@@ -1,23 +1,13 @@
 import pandas
 import requests
+# Import function definitions
+import functions
 
-api = "https://api.pro.coinbase.com"
+# On start
+try:
+	global data
+	data = load_data()
 
-def load_data():
-	
-
-def get_price():
-	try:
-		request = requests.get(api+"/products/btc-usd/ticker")
-		data = request.json()
-		time = data["time"][0:10] + " " + data["time"][11:19]
-		price = f'{float(data["price"]):.2f}'
-	
-		# Format is timestamp, numeric(7,2)
-		# ('YYYY-MM-DD HH:MM:SS', xxxxx.yy)
-		# TODO: Fix SQL syntax
-		#cursor.execute("INSERT INTO btc_price VALUES (\'" + time + "\', " + price + ");")
-	
-	except:
-		
-
+except IOError:
+	global data
+	data = pandas.DataFrame(columns=["Time","Price"])
