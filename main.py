98d6a5430896e1a1eed34e5e849e2014ebe24b4e
danieltmc <me@danieltmc.com>
2019-12-27 19:02:00 -0500

    Add try-except logic for saving data before exit

--- main.py
+++ main.py
@@ -3,11 +3,18 @@ import requests
 # Import function definitions
 import functions
 
-# On start
 try:
-	global data
-	data = load_data()
-
-except IOError:
-	global data
-	data = pandas.DataFrame(columns=["Time","Price"])
+	# On start
+	try:
+		global data
+		# Load Pickled DataFrame
+		data = load_data()	
+	except IOError:
+		global data
+		# Create DataFrame if one does not already exist
+		data = pandas.DataFrame(columns=["Time","Price"])
+# On exit
+except KeyboardInterrupt:
+	print("Saving data before exit...")
+	save_data(data)
+	
