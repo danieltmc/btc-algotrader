3527730e87d88032309dc1b6cb42e7c06dae931e
danieltmc <me@danieltmc.com>
2020-01-02 18:36:00 -0500

    Create functions to get last price or all open prices

--- functions.py
+++ functions.py
@@ -3,6 +3,7 @@ import pickle
 import requests
 
 api = "https://api.pro.coinbase.com"
+btc_usd = "/products/btc-usd"
 sandbox_api = "https://api-public.sandbox.pro.coinbase.com"
 
 def create_df():
@@ -21,11 +22,29 @@ def save_data(dataframe):
 	print("Pandas DataFrame saved")
 	return
 
-def get_price():
-	request = requests.get(api+"/products/btc-usd/ticker")
-	data = request.json()
-	time = data["time"][0:10] + " " + data["time"][11:19]
-	price = f'{float(data["price"]):.2f}'
+def book_price():
+	# Fetch all open orders
+	request = requests.get(api + btc_usd + "/book")
+	json = request.json()
+	asking = json["asks"]
+	total_weight = float(0)
+	# Sum the total order volume of open sales
+	for i in range(len(asking)):
+		total_weight += float(list[i][1])
+	avg_price = float(0)
+	# Sum the total prices, accounting for the percentage of all open sales
+	for i in range(len(asking)):
+		relative_weight = float(list[i][1]) / total_weight
+		ask_price = float(list[i][0])
+		avg_price += ask_price * relative_weight
+	# Calculate the weighted average of all open sales
+	avg_price = avg_price / total_weight
 
-	# Format is timestamp, numeric(7,2)
-	# ('YYYY-MM-DD HH:MM:SS', xxxxx.yy)
+def last_price():
+	# Request latest order (warning: can be a very small amount)
+	request = requests.get(api + btc_usd + "/ticker")
+	data = request.json()
+	# Time is in SQL timestamp format
+	time = data["time"][0:10] + " " + data["time"][11:19]
+	# Price is truncated to 2 decimal places
+	price = f'{float(data["price"]):.2f}'
