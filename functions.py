db9a7be865f94a46c76271622fbf134a70b07636
danieltmc <me@danieltmc.com>
2020-01-05 14:07:00 -0500

    Fix book_price list reference and add query for time when checking orders

--- functions.py
+++ functions.py
@@ -26,22 +26,28 @@ def save_data(dataframe):
 
 # Fetch all open orders
 def book_price():
-	request = requests.get(api + btc_usd + "/book")
-	json = request.json()
+	# Make requests first to minimize delay between time and price
+	time_request = requests.get(api + btc_usd + "/time")
+	order_request = requests.get(api + btc_usd + "/book?level=2") # Level 2 gives top 50 open orders
+	# Time is in SQL timestamp format
+	time_json = time_request.json()
+	time = time_json["iso"][0:10] + " " + time_json["time"][11:19]
 	# Only look at asking (selling) prices. Other contents of the dict are "sequence" (order #) and "bids" (buying prices)
-	asking = json["asks"]
+	order_json = order_request.json()
+	asking = order_json["asks"]
 	total_weight = float(0)
 	# Sum the total order volume of open sales
 	for i in range(len(asking)):
-		total_weight += float(list[i][1])
-	avg_price = float(0)
+		total_weight += float(asking[i][1])
+	weighted_price = float(0)
 	# Sum the total prices, accounting for the percentage of all open sales
 	for i in range(len(asking)):
-		relative_weight = float(list[i][1]) / total_weight
-		ask_price = float(list[i][0])
-		avg_price += ask_price * relative_weight
+		relative_weight = float(asking[i][1]) / total_weight
+		ask_price = float(asking[i][0])
+		weighted_price += ask_price * relative_weight
 	# Calculate the weighted average of all open sales
-	avg_price = avg_price / total_weight
+	weighted_price = weighted_price / total_weight
+	return(time, weighted_price)
 
 # Request latest order (warning: can be small trade volume)
 def last_price():
@@ -54,3 +60,4 @@ def last_price():
 	price = float(json["price"])
 	# Pass results as a tuple so time and price can be stored as a pair
 	return(time, price)
+
